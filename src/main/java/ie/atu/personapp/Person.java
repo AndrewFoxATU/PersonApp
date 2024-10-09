b663cb1c384c40c7104f1cffde9beb936030fabd
AndrewFoxATU <G00417373@atu.ie>
2024-10-09 15:22:06 +0100

    converted Product to Person

---
+++ src/main/java/ie/atu/personapp/Person.java
@@ -0,0 +1,38 @@
+package ie.atu.personapp;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    //
+    @NotBlank(message = "You must enter a name")
+    private String name;
+    //
+    @NotBlank(message = "You must enter a title")
+    private String title;
+    //
+    @Min(value=1, message=" ID must be greater or equal to 1")
+    private int id;
+    //
+    @Min(value=0, message="Age must be greater or equal to 0")
+    private int age;
+    //
+    @Email(message = "Email is not valid")
+    private String email;
+    //
+    @NotBlank(message = "You must enter a position")
+    private String position;
+    //
+    @NotBlank(message = "You must enter a department")
+    private String department;
+    //
+
+}
